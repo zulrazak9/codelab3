901ad63f040d8d50a19b66c32b4b437e2ece3518
zulrazak9 <149097796+zulrazak9@users.noreply.github.com>
2024-10-25 16:20:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+include "Controllers/ProductController.php";
+
+use Controller\ProductController ;
+
+$productController = new ProductController;
+
+echo $productController -> getAllProduct ();
